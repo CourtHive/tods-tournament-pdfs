0b710c3683c03191ee466e03591466efd5175cd4
Charles Allen <charles@courtHive.com>
2020-09-27 14:10:06 +0200

    generate tournament / drawDefinition

---
+++ test/serverSide/writePDF.test.js
@@ -0,0 +1,84 @@
+import fs from 'fs';
+import pdfmake from 'pdfmake';
+import { getDummy } from '../../src/generators/dummy';
+import {
+  drawDefinitionConstants,
+  tournamentEngine,
+  drawEngine,
+  fixtures,
+} from 'tods-competition-factory';
+import { tournamentRecordWithParticipants } from '../tournaments/tournamentWithParticipants';
+
+const { SEEDING_ITF } = fixtures;
+const { MAIN, ELIMINATION } = drawDefinitionConstants;
+
+const roboto = './node_modules/roboto-fontface/fonts/roboto';
+
+const fonts = {
+  Roboto: {
+    normal: `${roboto}/Roboto-Regular.woff`,
+    bold: `${roboto}/Roboto-Medium.woff`,
+    italics: `${roboto}/Roboto-Italic.woff`,
+    bolditalics: `${roboto}/Roboto-MediumItalic.woff`,
+  },
+};
+
+const pdfDir = './test/serverSide/pdfs';
+
+it('can generate dummy documentDefinition', () => {
+  const documentDefinition = getDummy();
+  expect(documentDefinition).not.toBeUndefined();
+
+  const printer = new pdfmake(fonts);
+  const pdfDoc = printer.createPdfKitDocument(documentDefinition);
+
+  const filename = 'dummy.pdf';
+  const filepath = `${pdfDir}/${filename}`;
+  pdfDoc.pipe(fs.createWriteStream(filepath));
+  pdfDoc.end();
+});
+
+it('can generate participant list', () => {
+  const { tournamentRecord, participants } = tournamentRecordWithParticipants({
+    startDate: '2020-01-01',
+    endDate: '2020-01-06',
+    participantsCount: 32,
+  });
+  expect(participants.length).toEqual(32);
+  expect(tournamentRecord).not.toBeUndefined();
+});
+
+it('can generate elimination draw structure', () => {
+  const drawSize = 8;
+  const { tournamentRecord, participants } = tournamentRecordWithParticipants({
+    startDate: '2020-01-01',
+    endDate: '2020-01-06',
+    participantsCount: 32,
+  });
+  expect(participants.length).toEqual(32);
+
+  const mainDrawParticipantIds = participants
+    .slice(0, drawSize)
+    .map(participant => participant.participantId);
+
+  tournamentEngine.setState(tournamentRecord);
+  drawEngine.newDrawDefinition();
+  drawEngine.attachPolicy({ policyDefinition: SEEDING_ITF });
+  drawEngine.setStageDrawSize({ stage: MAIN, drawSize });
+  drawEngine.generateDrawType({ stage: MAIN, drawType: ELIMINATION });
+  drawEngine.addDrawEntries({
+    stage: MAIN,
+    participantIds: mainDrawParticipantIds,
+  });
+  const { structures: mainStructures } = drawEngine.getDrawStructures({
+    stage: MAIN,
+    stageSequence: 1,
+  });
+  const [mainStructure] = mainStructures;
+  const { structureId: mainStructureId } = mainStructure;
+
+  drawEngine.automatedPositioning({ structureId: mainStructureId });
+
+  const { drawDefinition } = drawEngine.getState();
+  console.log(drawDefinition);
+});
---
+++ test/tournaments/tournamentWithParticipants.js
@@ -0,0 +1,28 @@
+import {
+  tournamentEngine,
+  resultConstants,
+  eventConstants,
+} from 'tods-competition-factory';
+
+const { SUCCESS } = resultConstants;
+const { SINGLES } = eventConstants;
+
+export function tournamentRecordWithParticipants({
+  startDate,
+  endDate,
+  participantsCount,
+  matchUpType = SINGLES,
+}) {
+  tournamentEngine.newTournamentRecord({ startDate, endDate });
+
+  const { participants } = tournamentEngine.generateFakeParticipants({
+    participantsCount,
+    matchUpType,
+  });
+  expect(participants.length).toEqual(participantsCount);
+
+  const result = tournamentEngine.addParticipants({ participants });
+  expect(result).toMatchObject(SUCCESS);
+
+  return { tournamentRecord: tournamentEngine.getState(), participants };
+}
